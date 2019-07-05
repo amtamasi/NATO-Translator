970c48580b15c51005003223809eca858da25b66
Anthony <anthonymtamasi@gmail.com>
2019-07-05 17:09:58 -0400

    Made functions for preprocessing and translating to NATO

--- nato.py
+++ nato.py
@@ -5,10 +5,46 @@ Created on Mon Jun 24 13:15:08 2019
 @author: amtam
 """
 
+def processString(input_str):
+    """
+    Pre-process the input string so that it can be converted to NATO alphabet. At the bare minimum, needs to change input_str to lowercase.
+    Parameters:
+        input_str (str): String input by user.
+    Returns:
+        str_processed (str): Processed string
+    """
+    input_str = input_str.lower()
+
+    return input_str
+
+def toNATO(string_in, nato):
+    """
+    Convert lowercase string to its NATO phonetic alphabet equivalent. If it is not a regular letter (e.g. punctuation), don't change it to anything.
+    Parameters:
+        string_in (str): String to be changed to NATO alphabet. Assumed to be in all lowercase characters.
+        nato (dict): Maps each alphabetic letter
+    Returns:
+        string_out (str): Converted string, in NATO alphabet form.
+    Variables:
+        output_array (list): Holds the strings of NATO equivalent for each letter in the string_in.
+    """
+    output_array = []
+    for char in string_in:
+        try:
+            new_char = nato[char]
+        except:
+            new_char = char
+            
+        output_array.append(new_char)
+
+    string_out = ' '.join(output_array)
+    return string_out
+
+
 def main():
     input_str = input('Enter a string: ')
     print()
-    output_array = []
+
     nato = {'a': 'Alfa',
             'b': 'Bravo',
             'c': 'Charlie',
@@ -37,18 +73,9 @@ def main():
             'z': 'Zulu'
             }
     
-    input_str = input_str.lower()
     
-    for char in input_str:
-        try:
-            new_char = nato[char]
-        except:
-            new_char = char
-            
-        output_array.append(new_char)
-            
-            
-    output_str = ' '.join(output_array)
+    input_str_processed = processString(input_str)
+    output_str = toNATO(input_str, nato)
     print(output_str)
             
             
